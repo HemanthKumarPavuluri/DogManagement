aa8b7d1fa3658fb9a59c82091db2afe3d2221fee
Hemanth_Kumar <phk.phk007@gmail.com>
2024-04-16 21:56:12 -0500

    intial commit

---
+++ src/main/java/com/hemanthProject/DMS/repository/TrainerRepository.java
@@ -0,0 +1,9 @@
+package com.hemanthProject.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hemanthProject.DMS.Models.Trainer;
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+}
